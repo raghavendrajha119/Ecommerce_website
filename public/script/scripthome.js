fa283687d88fc06a4cbb39da30b389e95e0370c1
raghavendrajha119 <raghavendrajha119@gmail.com>
2023-07-28 23:28:17 +0530

    Emplemented search function and categories

--- public/script/scripthome.js
+++ public/script/scripthome.js
@@ -82,16 +82,95 @@ document.getElementById('scrollToTopBtn').addEventListener('click', scrollToTop)
 // cookies handling
 let cookies = document.cookie;
 let cookie = cookies.split("=");
-if (cookies !== "" ) {
+if (cookies !== "") {
   let login = document.getElementById('login');
   let register = document.getElementById('register');
   login.style.display = 'none';
   register.style.display = 'none';
 }
-else{
+else {
   let logout = document.getElementById('logout');
   let cart = document.getElementById('cart');
   logout.style.display = 'none';
   cart.style.display = 'none';
   dashboard.style.display = 'none';
+}
+// emplementing search operation
+let search = document.getElementById('search');
+search.addEventListener("keyup", (event) => {
+  const { value } = event.target;
+  const searchQuery = value.toLowerCase();
+  let products = document.querySelector('.products');
+  products.innerHTML = '';
+  async function fetchProducts(url) {
+    try {
+      let data = await fetch(url);
+      let response = await data.json();
+      const ids = [];
+      for (let i = 0; i < response.length; i++) {
+        let description = response[i].description;
+        let title = response[i].title;
+        let categories = response[i].category;
+        let titlecheck = title.toLowerCase();
+        let desccheck = description.toLowerCase();
+        let catcheck = categories.toLowerCase();
+        if ((titlecheck.includes(searchQuery)) || (desccheck.includes(searchQuery)) || (catcheck.includes(searchQuery))) {
+
+          if (ids.includes(response[i].id)) {
+
+          }
+          else {
+            products.innerHTML += `
+            <div class="card">
+                <h3 class="Title">${response[i].title}</h3>
+                <h4 class="category">${response[i].category}</h4>
+                <img src="${response[i].image}" alt="image" class="image">
+                <p class="description">${description.length > 20 ? description.substring(0, 80).concat('...more') : description}</p>
+                <h5 class="price">$${response[i].price}</h5>
+                <button class="cart" onclick="addToCart(${response[i].id})">Add to cart</button>
+            </div>
+        `;
+          }
+          ids.push(response[i].id);
+        }
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  fetchProducts('https://fakestoreapi.com/products');
+});
+// emplementing categories
+let categories = document.getElementsByClassName('Cat');
+for (const category of categories){
+  category.addEventListener('click',()=>{
+    let products = document.querySelector('.products');
+    products.innerHTML = '';
+    async function fetchProducts(url) {
+      try {
+        let data = await fetch(url);
+        let response = await data.json();
+        for (let i = 0; i < response.length; i++) {
+          let description = response[i].description;
+          let catname = category.textContent.toLowerCase();
+          if (response[i].category.includes(catname)){
+            products.innerHTML += `
+            <div class="card">
+                <h3 class="Title">${response[i].title}</h3>
+                <h4 class="category">${response[i].category}</h4>
+                <img src="${response[i].image}" alt="image" class="image">
+                <p class="description">${description.length > 20 ? description.substring(0, 80).concat('...more') : description}</p>
+                <h5 class="price">$${response[i].price}</h5>
+                <button class="cart" onclick="addToCart(${response[i].id})">Add to cart</button>
+            </div>
+        `;
+          }
+        }
+      }catch (error) {
+        console.error('Error:', error);
+      } 
+      }
+      fetchProducts('https://fakestoreapi.com/products');
+  })
+
 }
